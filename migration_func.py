9dc0f80d9def665e742e5c779be18cfd4bb7e21b
Luciano Raso <luciofaso@gmail.com>
2018-09-06 08:53:03 +0200

    minor changes, renaming, to be pushed

---
+++ migration_func.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+
+def select_years (df:pd.DataFrame, column_dates:str, years:list):
+    """" Select the years from a larger datasests """
+
+    df_out = pd.DataFrame()
+    for year in years:
+        df_year = df [df[column_dates]== year]
+        df_out = df_out.append(df_year,ignore_index=True)
+    return(df_out)
+
+
+def order_by_nan(df:pd.DataFrame):
+    """Order dataframe (row and columns) according to presence of data, i.e. more data row-columns on top-left"""
+
+    new_index = df.isnull().sum(1).sort_values().index
+    new_col_index = df.isnull().sum().sort_values().index
+    df = df.reindex(new_index)
+    df = df.reindex(columns=new_col_index)
+    return df
+
+
+def smart_cut(df:pd.DataFrame, threshold=0, weight_col=1):
+    """Drop columns and raws with many nan, minimizing the loss of informative data-points
+    
+    Args: 
+        threshold : percentage of nan left in the dataframe
+        wheight_col : importance of columns, to penalize dropping of columns more (weight>1) or less (weight<1)
+    
+    Returns:
+        df (pd.DataFrame): clean dataframe 
+    """
+
+    while (df.isnull().sum().sum())/(df.shape[0] * df.shape[1]) > threshold:
+
+        worst_row = np.argmax(df.isnull().sum(1))
+        worst_row_value = np.max(df.isnull().sum(1))
+
+        worst_col = np.argmax(df.isnull().sum(0))
+        worst_col_value = np.max(df.isnull().sum(0))
+
+        # criterium : minimize loss of valid data-points
+        if (df.shape[1] - worst_row_value) <= (weight_col*(df.shape[0] - worst_col_value)):
+            df = df.drop(worst_row)
+        else:
+            df = df.drop(worst_col,axis=1)
+
+    return df
+
+
+
+def sel_regressor(df:pd.DataFrame, col_target:str):
+    """Given a dataframe and the target column, create matrix of regressors X and target y"""
+    
+    y = df[col_target].values
+    X = df.drop([col_target],axis=1).values
+    name_inputs = df.drop([col_target],axis=1).columns
+    return X,y,name_inputs
+
+
+
--- readme.md
+++ readme.md
@@ -1,14 +1,19 @@
 # Migration Data Analysis
 
 Analysis of Migration Data, inference of relationships with other variables.
+---
+This repository contains:
 
-* Data of international migration can be get [here]()
-* Socio-economic indicators gathered by the World Bank can be get [here] ()
-
+* Exploratory Data Analysis of emigration and the socio-economic data of the WB indicators, at [EmigrationEDA.ipynb](https://github.com/luciofaso)
+* A stochastic model that quantify the relationship between emigration and the WB indicators, at [EmigrationModel.ipynb](https://github.com/luciofaso)
 
 
 ## Authors
-* **Luciano Raso** - [Personal Page](https://www.tudelft.nl/en/tpm/about-the-faculty/departments/multi-actor-systems/people/researchers/dr-ir-lr-luciano-raso/) -- [git-hub repository](https://github.com/luciofaso) -- [Academic Activity](https://scholar.google.com/citations?user=_82Ogc8AAAAJ&hl=en&oi=ao).
+* **Luciano Raso** - [Personal Website](www.lraso.com), [@TU Delft](https://www.tudelft.nl/en/tpm/about-the-faculty/departments/multi-actor-systems/people/researchers/dr-ir-lr-luciano-raso/) -- [git-hub repository](https://github.com/luciofaso) -- [Academic Activity](https://scholar.google.com/citations?user=_82Ogc8AAAAJ&hl=en&oi=ao).
+
+## Data Availability
+* Data of international migration can be get [here](http://databank.worldbank.org/data/reports.aspx?source=global-bilateral-migration)
+* Socio-economic indicators gathered by the World Bank (WB) can be get [here] (), or [here](https://www.kaggle.com/worldbank/world-development-indicators/version/2#Indicators.csv)
 
 
 ## License
